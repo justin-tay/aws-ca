cd353053eb0997e856ef9fcecc0b9ddeefcacf93
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-07-29 11:56:58 +0800

    update

--- test/aws-ca.test.ts
+++ test/aws-ca.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCa from '../lib/aws-ca-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as AwsCa from "../lib/AwsCaStack";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/aws-ca-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsCa.AwsCaStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("API Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AwsCa.AwsCaStack(app, "MyTestStack");
+  // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties("AWS::SQS::Queue", {
+    VisibilityTimeout: 300,
+  });
 });
