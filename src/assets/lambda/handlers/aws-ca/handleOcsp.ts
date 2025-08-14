3a56c8f83d7617642e1f00a85162b066bc3098a9
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-08-14 15:46:42 +0800

    update

--- src/assets/lambda/handlers/aws-ca/handleOcsp.ts
+++ src/assets/lambda/handlers/aws-ca/handleOcsp.ts
@@ -1,13 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { OCSPRequest } from 'pkijs';
+import {
+  BasicOCSPResponse,
+  OCSPRequest,
+  OCSPResponse,
+  ResponseBytes,
+  SingleResponse,
+  id_PKIX_OCSP_Basic,
+} from 'pkijs';
+import { Enumerated, OctetString, Primitive } from 'asn1js';
+import { getConfig } from './ca/getConfig';
+import { loadSubCa } from './ca/loadSubCa';
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { getDynamoDBDocumentClient } from './ca/getDynamoDBDocumentClient';
+import CertificateStatus from './ca/CertificateStatus';
 
 export async function handleOcsp(
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> {
   const { headers, body, httpMethod } = event;
-  let ocsp;
+  let ocspRequest;
   if (httpMethod === 'GET') {
-    ocsp = OCSPRequest.fromBER(
+    ocspRequest = OCSPRequest.fromBER(
       Buffer.from(
         event.path.substring(event.path.lastIndexOf('/') + 1),
         'base64',
@@ -35,18 +48,93 @@ export async function handleOcsp(
         body: 'Expected binary body',
       };
     }
-    ocsp = OCSPRequest.fromBER(Buffer.from(body, 'base64')); // binary der
+    ocspRequest = OCSPRequest.fromBER(Buffer.from(body, 'base64')); // binary der
   } else {
     return {
       statusCode: 405,
       body: 'Method Not Allowed',
     };
   }
-  ocsp.tbsRequest.requestList.forEach((req) => {
-    console.log(req.reqCert.serialNumber);
+
+  const subCa = await loadSubCa();
+  const docClient = getDynamoDBDocumentClient();
+
+  const basicOcspResponse = new BasicOCSPResponse();
+
+  basicOcspResponse.tbsResponseData.responderID = subCa.certificate?.subject;
+  basicOcspResponse.tbsResponseData.producedAt = new Date();
+
+  for (const request of ocspRequest.tbsRequest.requestList) {
+    const certID = request.reqCert;
+    const serialNumber = certID.serialNumber;
+
+    const certificateCommand = new QueryCommand({
+      TableName: getConfig().caIndexTableName,
+      KeyConditionExpression:
+        'IssuerName = :issuerName and SerialNumber = :serialNumber',
+      ExpressionAttributeValues: {
+        ':issuerName': getConfig().subCaName,
+        ':serialNumber': serialNumber,
+      },
+      ConsistentRead: true,
+    });
+
+    const certificateResponse = await docClient.send(certificateCommand);
+
+    let tagNumber = 2; // unknown
+    let valueHex;
+    if (
+      certificateResponse &&
+      certificateResponse.Count &&
+      certificateResponse.Count > 0 &&
+      certificateResponse.Items
+    ) {
+      if (certificateResponse.Items[0].Status === CertificateStatus.Revoked) {
+        tagNumber = 1; // revoked
+        // TODO create RevokedInfo
+      } else if (
+        certificateResponse.Items[0].Status === CertificateStatus.Expired
+      ) {
+        //tagNumber = 2; // unknown
+      } else {
+        tagNumber = 0; // good
+      }
+    }
+
+    const response = new SingleResponse({
+      certID,
+    });
+    response.certStatus = new Primitive({
+      idBlock: {
+        tagClass: 3,
+        tagNumber,
+      },
+      valueHex,
+    });
+    response.thisUpdate = new Date();
+
+    basicOcspResponse.tbsResponseData.responses.push(response);
+  }
+
+  const basicOcspResponseRaw = basicOcspResponse.toSchema().toBER(false);
+
+  const ocspResponse = new OCSPResponse({
+    responseStatus: new Enumerated({ value: 0 }), // success
+    responseBytes: new ResponseBytes({
+      responseType: id_PKIX_OCSP_Basic,
+      response: new OctetString({ valueHex: basicOcspResponseRaw }),
+    }),
   });
+
+  const ocspResponseRaw = ocspResponse.toSchema().toBER();
+
   return {
-    statusCode: 500,
-    body: 'Internal Server Error',
+    headers: {
+      'Content-Type': 'application/ocsp-response',
+      'Content-Length': ocspResponseRaw.byteLength,
+    },
+    statusCode: 200,
+    body: Buffer.from(ocspResponseRaw).toString('base64'),
+    isBase64Encoded: true,
   };
 }
