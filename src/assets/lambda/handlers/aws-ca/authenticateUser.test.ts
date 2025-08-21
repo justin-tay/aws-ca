6cde4321161e9654754b0da0a41b6b5c682ffa48
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-08-21 23:21:13 +0800

    update crypto

--- src/assets/lambda/handlers/aws-ca/authenticateUser.test.ts
+++ src/assets/lambda/handlers/aws-ca/authenticateUser.test.ts
@@ -1,11 +1,41 @@
+import {
+  AuthFlowType,
+  InitiateAuthCommand,
+  InitiateAuthCommandOutput,
+} from '@aws-sdk/client-cognito-identity-provider';
 import { authenticateUser } from './authenticateUser';
+const mockSend = vi.fn();
+
+vi.mock('@aws-sdk/client-cognito-identity-provider', async () => ({
+  ...(await vi.importActual('@aws-sdk/client-cognito-identity-provider')),
+  CognitoIdentityProviderClient: vi
+    .fn()
+    .mockImplementation(() => ({ send: mockSend })),
+}));
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
 
 describe('authenticateUser', () => {
   it('should authenticate', async () => {
-    await authenticateUser({
-      username: 'user',
-      password: 'Password1#',
-      clientId: 'mo38425hv6aean78irt4ciju5',
+    mockSend.mockImplementation((command) => {
+      if (command instanceof InitiateAuthCommand) {
+        expect(command.input.AuthFlow).toBe(AuthFlowType.USER_PASSWORD_AUTH);
+        expect(command.input.AuthParameters?.USERNAME).toBe('user');
+        expect(command.input.AuthParameters?.PASSWORD).toBe('Password1#');
+        expect(command.input.ClientId).toBe('mo38425hv6aean78irt4ciju5');
+        const result: Partial<InitiateAuthCommandOutput> = {};
+        return Promise.resolve(result);
+      }
+      throw Error();
     });
+    await expect(
+      authenticateUser({
+        username: 'user',
+        password: 'Password1#',
+        clientId: 'mo38425hv6aean78irt4ciju5',
+      }),
+    ).resolves.not.toThrowError();
   });
 });
--- src/assets/lambda/handlers/aws-ca/authorize.test.ts
+++ src/assets/lambda/handlers/aws-ca/authorize.test.ts
@@ -1,10 +1,41 @@
+import {
+  AuthFlowType,
+  InitiateAuthCommand,
+  InitiateAuthCommandOutput,
+} from '@aws-sdk/client-cognito-identity-provider';
 import { authorize } from './authorize';
+
+const mockSend = vi.fn();
+
+vi.mock('@aws-sdk/client-cognito-identity-provider', async () => ({
+  ...(await vi.importActual('@aws-sdk/client-cognito-identity-provider')),
+  CognitoIdentityProviderClient: vi
+    .fn()
+    .mockImplementation(() => ({ send: mockSend })),
+}));
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
 
 describe('authorize', () => {
   it('should authorize', async () => {
-    await authorize({
-      authorizationHeader: 'Basic dXNlcjpQYXNzd29yZDEj',
-      clientId: 'mo38425hv6aean78irt4ciju5',
+    mockSend.mockImplementation((command) => {
+      if (command instanceof InitiateAuthCommand) {
+        expect(command.input.AuthFlow).toBe(AuthFlowType.USER_PASSWORD_AUTH);
+        expect(command.input.AuthParameters?.USERNAME).toBe('user');
+        expect(command.input.AuthParameters?.PASSWORD).toBe('Password1#');
+        expect(command.input.ClientId).toBe('mo38425hv6aean78irt4ciju5');
+        const result: Partial<InitiateAuthCommandOutput> = {};
+        return Promise.resolve(result);
+      }
+      throw Error();
     });
+    await expect(
+      authorize({
+        authorizationHeader: 'Basic dXNlcjpQYXNzd29yZDEj',
+        clientId: 'mo38425hv6aean78irt4ciju5',
+      }),
+    ).resolves.not.toThrowError();
   });
 });
